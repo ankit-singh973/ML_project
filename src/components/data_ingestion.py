3dce6171400d67c180517218b1baa32a4ee88187
ankit-singh973 <singhankit973@gmail.com>
2023-03-22 02:31:44 +0530

    Data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,53 @@
+import os
+import sys
+sys.path.insert(0, 'D:\Storage\mlproject\src') #sys.path.append('D:\Storage\mlproject\src') 
+from exception import CustomException
+from logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:  
+# three paths for data saving:          
+#                                        #folder name  #file name
+    train_data_path: str = os.path.join('artifacts', "train.csv") #1
+    test_data_path: str = os.path.join('artifacts', "test.csv") #2
+    raw_data_path: str = os.path.join('artifacts', "data.csv") #3
+
+class DataIngestion: #when we initialize it
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig #the above 3 paths gets saved in this class variable
+
+    def initiate_data_ingestion(self):
+        logging.info("Executing data ingestion method or component")
+        try:
+            df = pd.read_csv("notebook\data\stud.csv")
+            logging.info("Dataset imported/ loaded")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
+            df.to_csv(self.ingestion_config.raw_data_path, index = False, header= True)
+
+            logging.info("train test split initiated")
+            train_set,test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index= False, header = True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+
+            logging.info("ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
--- src/logger.py
+++ src/logger.py
@@ -18,6 +18,3 @@ logging.basicConfig(
 
 )
 
-if __name__ == "__main__":
-    logging.info("logging started")
-
